453a98b35105b1585f87c5b2537c92469440d8aa
Abiji-2020 <89566028+Abiji-2020@users.noreply.github.com>
2021-08-28 10:00:51 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -30,7 +30,7 @@ from bot import (
 async def convert_video(video_file, output_directory, total_time, bot, message, target_percentage, isAuto):
     # https://stackoverflow.com/a/13891070/4723940
     out_put_file_name = output_directory + \
-        "/" + str(round(time.time())) + ".mkv"
+        "/" + str(round(time.time())) + ".mp4"
     progress = output_directory + "/" + "progress.txt"
     with open(progress, 'w') as f:
       pass
